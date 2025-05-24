daa5a4a8ac3525d7c3c36491bc7b201fb421c0c8
Sahifa Hashmi <sahifahashmi786@yahoo.com>
2025-05-24 14:46:30 +0500

    Modeified Patient_Voice

--- voice_of_the_patient.py
+++ voice_of_the_patient.py
@@ -1,5 +1,11 @@
 #STEP 1: SETUP AUDIO RECORDER (FFMPEG & PORTAUDIO)
+import logging
+import speech_recognition as sr
+from pydub import AudioSegment
+from io import BytesIO
 
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 #STEP 2: SETUP SPEECH-TO-TECT STT MODEL FOR TRANSCRIPTION
